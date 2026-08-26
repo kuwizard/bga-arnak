5f08627aef43f5ee7e5ff2a6a4b256e57e61bcde
Remi Bertin <remiber@free.fr>
2026-08-26 11:52:27 +0200

    Refactoring : insert for research_bonus and temple_tiles

--- modules/sql_wrappers.php
+++ modules/sql_wrappers.php
@@ -588,6 +588,13 @@ class SqlWrapper {
     $this->game->notifyAllPlayers("removeResearchToken", "", array("tokenId" => $id));
   }
 
+  public function createResearchTokens($positions, $bonuses) {
+    foreach ($positions as $idx => $position) {
+      $bonus = $bonuses[$idx];
+      $this->game->DbQuery("INSERT INTO research_bonus (track_pos, bonus_type) VALUES ($position, '$bonus')");
+    }
+  }
+
   public function getAllTempleTiles () {
     $tiles = $this->game->getCollectionFromDb("SELECT idtemple_tile id, amt amt FROM temple_tile");
     foreach ($tiles as $idx => $tile) {
@@ -612,6 +619,12 @@ class SqlWrapper {
         "i18n" => ["colorText"]
       )
     );
+  }
+
+  public function createTempleTiles($amt) {
+    for ($tileId = 1; $tileId <= 6; ++$tileId) {
+      $this->game->DbQuery("INSERT INTO temple_tile (idtemple_tile, amt) VALUES ($tileId, $amt)");
+    }
   }
 }
 ?>
